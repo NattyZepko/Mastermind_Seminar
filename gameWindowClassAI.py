44eecb6da3cfed4729602be282decd679885576b
eyalItzhak <62293316+eyalItzhak@users.noreply.github.com>
2022-11-14 12:15:52 +0200

    changed to classes
    Made changes to the game window file, made it a class instead of a function, and updated the animation for the window population.
    
    TO DO: fix the game timing (don't start the last gmae), and add manual player mode
    
    Co-Authored-By: Natty Zepko <Natty.zepko@gmail.com>

---
+++ gameWindowClassAI.py
@@ -0,0 +1,64 @@
+import tkinter.messagebox
+from tkinter import *
+import bh
+import time  # AND SPACE!
+
+
+def get_digit(number, n):
+    stringNumber = str(number)
+    return stringNumber[n]
+
+class Game_AI:
+
+     def __init__(self,game_count=1, zero_included=0, num_of_digits=4):
+        self.gameCount=game_count
+        self.zeroIncluded=zero_included
+        self.numOfDigits=num_of_digits 
+        self.sleeping_Time = 1000
+        self.root2 = Tk()
+
+        bh.Zero = zero_included
+        bh.NumberOfGames = game_count
+        bh.NumberOfDigits = num_of_digits
+
+     def startGame(self):
+        for idx_game in range(self.gameCount):
+            self.currentGame = bh.BH(0, self.numOfDigits)
+            self.create_header(idx_game)
+        # After the windows are built, we launch it
+        self.root2.mainloop()
+
+
+
+     def populate_window(self):
+        self.allGuesses = self.currentGame.getGuesses()
+        self.NB=self.currentGame.getNBs()
+        self.NH=self.currentGame.getNHs()
+        self.populateAllRows(1, 0)
+
+
+     def populateAllRows(self, rowPosition, guess_index):
+        SpinBoxList = []
+        current_guess = self.allGuesses[guess_index]
+        for column_idx in range(self.numOfDigits):  
+            SpinBoxList.append(Spinbox(self.root2, width=2))
+            SpinBoxList[column_idx].grid(row=rowPosition, column=column_idx)
+            SpinBoxList[column_idx].insert(0, get_digit(current_guess, column_idx))
+            SpinBoxList[column_idx].config(state=DISABLED)
+        guess_result_label = Label(self.root2, text="Bull:"+str(self.NB[guess_index])+" Hits:" + str(self.NH[guess_index]))
+        guess_result_label.grid(row=rowPosition, column=self.numOfDigits)
+        if (guess_index<len(self.allGuesses)-1):
+            guess_result_label.after(self.sleeping_Time, lambda: self.populateAllRows(rowPosition+1, guess_index+1))
+        
+
+        
+
+     def create_header(self, idx_game):
+        ans_label=[]
+        for i in range(self.numOfDigits):
+            ans_label.append(Label(self.root2,text='?'))
+            ans_label[i].grid(row=0,column=i)
+        game_num_label= Label(self.root2,text="game # "+str(idx_game+1))    
+        game_num_label.grid(row=0,column=self.numOfDigits)
+        game_num_label.after(self.sleeping_Time, self.populate_window)
+
--- gamewindow.py
+++ gamewindow.py
@@ -2,71 +2,57 @@ import tkinter.messagebox
 from tkinter import *
 import bh
 import time  # AND SPACE!
+import gameWindowClassAI
+
+# gameCount=1
+# zeroIncluded=0
+# numOfDigits = 4
+# SleepingTime=0
+
 
 
 # ### Return the n-th digit of a number
-def get_digit(number, n):
-    stringNumber = str(number)
-    return stringNumber[n]
 
+
+# def create_header(idx_game,root2):
+#     ans_label=[]
+#     for i in range(numOfDigits):
+#         ans_label.append(Label(root2,text='?'))
+#         ans_label[i].grid(row=0,column=i)
+#     game_num_label= Label(root2,text="game # "+str(idx_game+1))    
+#     game_num_label.grid(row=0,column=numOfDigits)
+
+
+# def run_AI_games(root2):
+#     # for idx_game in range(gameCount):
+#         create_header(0,root2)
+
+
+   
+
+def run_menual_games():
+    return    
 
 def StartGame(player_type, game_count=1, zero_included=0, num_of_digits=4):
-    # gameWindow = Toplevel()
-    root2 = Tk()
-    bh.Zero = zero_included
-    bh.NumberOfGames = game_count
-    bh.NumberOfDigits = num_of_digits
-    Sleeping_Time = 0
+    # # gameWindow = Toplevel()
+    # root2 = Tk()
+    # bh.Zero = zero_included
+    # bh.NumberOfGames = game_count
+    # bh.NumberOfDigits = num_of_digits
+    # # updating blobal parameters
+    # gameCount=game_count
+    # zeroIncluded=zero_included
+    # numOfDigits=num_of_digits
+    # SleepingTime = 0
 
     if player_type == "AI":
-        guess_count_per_game = []
-        for game_num in range(game_count):  # For each game:
-            picturesOfQuestionMarks = []
-            for j in range(num_of_digits):
-                picturesOfQuestionMarks.append(Label(root2, text="?"))
-                picturesOfQuestionMarks[j].grid(row=0, column=j)
-
-            CurrentGame = bh.BH(number=0, numberOfDigits=num_of_digits)  # Current Game object holds the current game
-            guess_count_per_game.append(len(CurrentGame.getGuesses()))
-            allGuesses = CurrentGame.getGuesses()  # Get all guesses
-            allNB = CurrentGame.getNBs()  # Get all Bull (right color, wrong spot)
-            allNH = CurrentGame.getNHs()  # Get all Hits (right color, right spot)
-            print(allGuesses)  # SANITY CHECK
-            Game_number_Label = Label(root2, text="Game number " + str(game_num + 1))  # Label with current game number
-            Game_number_Label.grid(row=0, column=num_of_digits + 1)  # Put it after the question marks
-            for Guess_index, guess in enumerate(allGuesses):  # For each guess
-                time.sleep(Sleeping_Time)
-                digitsGuess = []  # A list of int variables
-                digitSpinBoxes = []  # A list of the Spin Box objects, controlled by the int variables
-                for j in range(num_of_digits):  # j iterates items in a row
-                    digitsGuess.append(IntVar())  # make an int variable
-                    if zero_included:
-                        digitsGuess[j].set(0)
-                    else:
-                        digitsGuess[j].set(1)
-                    digitSpinBoxes.append(Spinbox(root2, width=2))# , state=DISABLED))  # Create a spin box
-                    digitSpinBoxes[j].grid(row=Guess_index+1, column=j)  # Put it on the screen, index+1 because we want the count to start from 1.
-
-                # ### ANIMATING A GUESS:
-                time.sleep(Sleeping_Time)  # After drawing the guess line, wait
-                for j in range(num_of_digits):
-                    digitSpinBoxes[j].insert(0,get_digit(guess, j)) 
-                    digitSpinBoxes[j].config(state=DISABLED)# ###run when it is not disable and fix*
-                Guess_Result_Label = Label(root2, text="Bull:"+str(allNB[Guess_index])+" Hits:" + str(allNH[Guess_index]))
-                Guess_Result_Label.grid(row=Guess_index+1, column=num_of_digits + 1)
-            for index, pic in enumerate(picturesOfQuestionMarks):
-                pic.config(text=get_digit(guess, index))  # REVEAL THE ANSWER
-            Game_Result_Label = Label(root2, text="Win in " + str(len(allGuesses)) + " guesses.")  # Show number of guesses
-            Game_Result_Label.grid(row=len(allGuesses) + 2, column=1, columnspan=5)
-            tkinter.messagebox.showinfo("Game Ended", "Game #" + str(game_num+1) + " ended. Press OK to proceed.")
-            # CLEAN THE WINDOW
-            for widget in root2.winfo_children():
-                widget.destroy()
-        avg = round(sum(guess_count_per_game)/len(guess_count_per_game), 3)  # At most, 3 decimal places
-        Total_Result_Label = Label(root2, text = "    After " + str(game_count) + " games:\nThe average amount of guesses is: " + str(avg) + "   ")
-        Total_Result_Label.grid(row=0,column=0)
+        print("AI")
+        # run_AI_games(root2)
+        my_game=gameWindowClassAI.Game_AI(game_count,zero_included,num_of_digits)
+        my_game.startGame()
 
     else:
-        print("TO DO")
+        print("TO DO") 
+        run_menual_games()
 
-    root2.mainloop()
+    # root2.mainloop()
