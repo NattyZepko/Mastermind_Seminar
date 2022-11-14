7889ea20edbed86d53f70f2b2a1b6982eb41b54d
Natty Zepko <natty.zepko@gmail.com>
2022-11-14 23:02:58 +0200

    Fixed names and conventions. Just fixed indentations, no changes to the code (yet)

--- gameWindowClassAI.py
+++ gameWindowClassAI.py
@@ -8,9 +8,13 @@ def get_digit(number, n):
     stringNumber = str(number)
     return stringNumber[n]
 
-class Game_AI:
 
-     def __init__(self,game_count=1, zero_included=0, num_of_digits=4):
+class GameAI:
+    def __init__(self,game_count=1, zero_included=0, num_of_digits=4):
+        self.NH = None
+        self.NB = None
+        self.allGuesses = None
+        self.currentGame = None
         self.gameCount=game_count
         self.zeroIncluded=zero_included
         self.numOfDigits=num_of_digits 
@@ -21,39 +25,33 @@ class Game_AI:
         bh.NumberOfGames = game_count
         bh.NumberOfDigits = num_of_digits
 
-     def startGame(self):
+    def startGame(self):
         for idx_game in range(self.gameCount):
             self.currentGame = bh.BH(0, self.numOfDigits)
             self.create_header(idx_game)
-        # After the windows are built, we launch it
+            # After the windows are built, we launch it
         self.root2.mainloop()
 
-
-
-     def populate_window(self):
+    def populate_window(self):
         self.allGuesses = self.currentGame.getGuesses()
         self.NB=self.currentGame.getNBs()
         self.NH=self.currentGame.getNHs()
         self.populateAllRows(1, 0)
 
-
-     def populateAllRows(self, rowPosition, guess_index):
+    def populateAllRows(self, row_position, guess_index):
         SpinBoxList = []
         current_guess = self.allGuesses[guess_index]
         for column_idx in range(self.numOfDigits):  
             SpinBoxList.append(Spinbox(self.root2, width=2))
-            SpinBoxList[column_idx].grid(row=rowPosition, column=column_idx)
+            SpinBoxList[column_idx].grid(row=row_position, column=column_idx)
             SpinBoxList[column_idx].insert(0, get_digit(current_guess, column_idx))
             SpinBoxList[column_idx].config(state=DISABLED)
         guess_result_label = Label(self.root2, text="Bull:"+str(self.NB[guess_index])+" Hits:" + str(self.NH[guess_index]))
-        guess_result_label.grid(row=rowPosition, column=self.numOfDigits)
-        if (guess_index<len(self.allGuesses)-1):
-            guess_result_label.after(self.sleeping_Time, lambda: self.populateAllRows(rowPosition+1, guess_index+1))
-        
+        guess_result_label.grid(row=row_position, column=self.numOfDigits)
+        if guess_index < len(self.allGuesses)-1:
+            guess_result_label.after(self.sleeping_Time, lambda: self.populateAllRows(row_position+1, guess_index+1))
 
-        
-
-     def create_header(self, idx_game):
+    def create_header(self, idx_game):
         ans_label=[]
         for i in range(self.numOfDigits):
             ans_label.append(Label(self.root2,text='?'))
--- gamewindow.py
+++ gamewindow.py
@@ -4,55 +4,18 @@ import bh
 import time  # AND SPACE!
 import gameWindowClassAI
 
-# gameCount=1
-# zeroIncluded=0
-# numOfDigits = 4
-# SleepingTime=0
 
-
-
-# ### Return the n-th digit of a number
-
-
-# def create_header(idx_game,root2):
-#     ans_label=[]
-#     for i in range(numOfDigits):
-#         ans_label.append(Label(root2,text='?'))
-#         ans_label[i].grid(row=0,column=i)
-#     game_num_label= Label(root2,text="game # "+str(idx_game+1))    
-#     game_num_label.grid(row=0,column=numOfDigits)
-
-
-# def run_AI_games(root2):
-#     # for idx_game in range(gameCount):
-#         create_header(0,root2)
-
-
-   
-
-def run_menual_games():
+def run_manual_games():
     return    
 
-def StartGame(player_type, game_count=1, zero_included=0, num_of_digits=4):
-    # # gameWindow = Toplevel()
-    # root2 = Tk()
-    # bh.Zero = zero_included
-    # bh.NumberOfGames = game_count
-    # bh.NumberOfDigits = num_of_digits
-    # # updating blobal parameters
-    # gameCount=game_count
-    # zeroIncluded=zero_included
-    # numOfDigits=num_of_digits
-    # SleepingTime = 0
 
+def StartGame(player_type, game_count=1, zero_included=0, num_of_digits=4):
     if player_type == "AI":
         print("AI")
-        # run_AI_games(root2)
-        my_game=gameWindowClassAI.Game_AI(game_count,zero_included,num_of_digits)
+        my_game = gameWindowClassAI.GameAI(game_count, zero_included, num_of_digits)
         my_game.startGame()
 
     else:
         print("TO DO") 
-        run_menual_games()
+        run_manual_games()
 
-    # root2.mainloop()
