d22dec03e37527737aae34b1f065a737c2a70409
eyalItzhak <62293316+eyalItzhak@users.noreply.github.com>
2022-11-14 10:08:41 +0200

    Alpha 0.1
    We filled the fields of the guesses with the appropriate digits for the guesses. Alpha version 0.1

--- gamewindow.py
+++ gamewindow.py
@@ -20,7 +20,7 @@ def StartGame(player_type, game_count=1, zero_included=0, num_of_digits=4):
 
     if player_type == "AI":
         guess_count_per_game = []
-        for i in range(game_count):  # For each game:
+        for game_num in range(game_count):  # For each game:
             picturesOfQuestionMarks = []
             for j in range(num_of_digits):
                 picturesOfQuestionMarks.append(Label(root2, text="?"))
@@ -32,7 +32,7 @@ def StartGame(player_type, game_count=1, zero_included=0, num_of_digits=4):
             allNB = CurrentGame.getNBs()  # Get all Bull (right color, wrong spot)
             allNH = CurrentGame.getNHs()  # Get all Hits (right color, right spot)
             print(allGuesses)  # SANITY CHECK
-            Game_number_Label = Label(root2, text="Game number " + str(i + 1))  # Label with current game number
+            Game_number_Label = Label(root2, text="Game number " + str(game_num + 1))  # Label with current game number
             Game_number_Label.grid(row=0, column=num_of_digits + 1)  # Put it after the question marks
             for Guess_index, guess in enumerate(allGuesses):  # For each guess
                 time.sleep(Sleeping_Time)
@@ -44,21 +44,21 @@ def StartGame(player_type, game_count=1, zero_included=0, num_of_digits=4):
                         digitsGuess[j].set(0)
                     else:
                         digitsGuess[j].set(1)
-                    digitSpinBoxes.append(Spinbox(root2, textvariable=get_digit(guess, j), width=2, state=DISABLED))  # Create a spin box
+                    digitSpinBoxes.append(Spinbox(root2, width=2))# , state=DISABLED))  # Create a spin box
                     digitSpinBoxes[j].grid(row=Guess_index+1, column=j)  # Put it on the screen, index+1 because we want the count to start from 1.
 
                 # ### ANIMATING A GUESS:
                 time.sleep(Sleeping_Time)  # After drawing the guess line, wait
                 for j in range(num_of_digits):
-                    digitsGuess[j].set(get_digit(guess, j))  # Set the spinbox value to match the digit of the guess
-                    digitSpinBoxes[j].config(textvariable=get_digit(guess, j))
+                    digitSpinBoxes[j].insert(0,get_digit(guess, j)) 
+                    digitSpinBoxes[j].config(state=DISABLED)# ###run when it is not disable and fix*
                 Guess_Result_Label = Label(root2, text="Bull:"+str(allNB[Guess_index])+" Hits:" + str(allNH[Guess_index]))
                 Guess_Result_Label.grid(row=Guess_index+1, column=num_of_digits + 1)
             for index, pic in enumerate(picturesOfQuestionMarks):
                 pic.config(text=get_digit(guess, index))  # REVEAL THE ANSWER
             Game_Result_Label = Label(root2, text="Win in " + str(len(allGuesses)) + " guesses.")  # Show number of guesses
             Game_Result_Label.grid(row=len(allGuesses) + 2, column=1, columnspan=5)
-            tkinter.messagebox.showinfo("Game Ended", "Game #" + str(i+1) + " ended. Press OK to proceed.")
+            tkinter.messagebox.showinfo("Game Ended", "Game #" + str(game_num+1) + " ended. Press OK to proceed.")
             # CLEAN THE WINDOW
             for widget in root2.winfo_children():
                 widget.destroy()
